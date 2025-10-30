618b1ae71a4d3be417ac92b96dcdae7037255c75
fi3ework <fi3ework@gmail.com>
2025-10-30 21:04:53 +0800

    fix(rslib): install playwright

--- tests/rslib/rspress.ts
+++ tests/rslib/rspress.ts
@@ -1,11 +1,14 @@
 import type { RunOptions } from '../../types';
-import { runInRepo } from '../../utils';
+import { $, runInRepo } from '../../utils';
 
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     repo: 'web-infra-dev/rspress',
     branch: process.env.RSPRESS ?? 'main',
+    beforeTest: async () => {
+      await $`pnpm playwright install --with-deps`;
+    },
     test: ['test'],
   });
 }
