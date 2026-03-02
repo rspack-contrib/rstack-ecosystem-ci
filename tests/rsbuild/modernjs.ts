6544793dbe0478d1689cd5249f0947fdbcf7f6b1
neverland <chenjiahan.jait@bytedance.com>
2026-03-02 17:33:34 +0800

    fix: test modern.js main branch

--- tests/rsbuild/modernjs.ts
+++ tests/rsbuild/modernjs.ts
@@ -12,8 +12,7 @@ export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     repo: 'web-infra-dev/modern.js',
-    // Using `v2` (the stable version) as `main` is still under development.
-    branch: process.env.MODERN_REF ?? 'v2',
+    branch: process.env.MODERN_REF ?? 'main',
     beforeInstall: async () => {
       if (isGitHubActions) {
         const modernJsDir = join(process.cwd(), 'workspace/modernjs/modern.js');
