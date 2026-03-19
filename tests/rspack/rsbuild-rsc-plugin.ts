e97e256a1080910167716e46755191c24d2e7456
Cong-Cong <dacongsama@live.com>
2026-03-19 11:15:08 +0800

    lint

--- tests/rspack/rsbuild-rsc-plugin.ts
+++ tests/rspack/rsbuild-rsc-plugin.ts
@@ -1,14 +1,14 @@
-import { runInRepo, $ } from '../../utils'
-import { RunOptions } from '../../types'
+import type { RunOptions } from '../../types';
+import { $, runInRepo } from '../../utils';
 
 export async function test(options: RunOptions) {
-    await runInRepo({
-        ...options,
-        repo: 'rstackjs/rsbuild-plugin-rsc',
-        beforeTest: async () => {
-            await $`pnpm run build`
-            await $`npx playwright install`
-        },
-        test: ['test'],
-    })
+  await runInRepo({
+    ...options,
+    repo: 'rstackjs/rsbuild-plugin-rsc',
+    beforeTest: async () => {
+      await $`pnpm run build`;
+      await $`npx playwright install`;
+    },
+    test: ['test'],
+  });
 }
