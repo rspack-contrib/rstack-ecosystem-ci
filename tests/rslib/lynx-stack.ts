4aa41a619c1e448bbb5bde5004ae70175453322c
neverland <chenjiahan.jait@bytedance.com>
2026-07-30 21:15:09 +0800

    fix

--- tests/rslib/lynx-stack.ts
+++ tests/rslib/lynx-stack.ts
@@ -1,13 +1,3 @@
-import type { RunOptions } from '../../types';
-import { runInRepo } from '../../utils';
+import { test } from '../rspack/lynx-stack';
 
-export async function test(options: RunOptions) {
-  await runInRepo({
-    ...options,
-    repo: 'lynx-family/lynx-stack',
-    branch: process.env.LYNX_STACK_REF ?? 'main',
-    build: 'pnpm turbo build',
-    // TODO(colinaaa): enable Lynx for Web tests
-    test: 'test',
-  });
-}
+export { test };
