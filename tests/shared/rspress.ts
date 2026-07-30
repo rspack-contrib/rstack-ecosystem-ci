bed9ebed2aebb40ca34c1845090aeb312fc988b9
neverland <chenjiahan.jait@bytedance.com>
2026-07-30 21:53:14 +0800

    chore: remove redundant Playwright installs

--- tests/shared/rspress.ts
+++ tests/shared/rspress.ts
@@ -1,14 +1,11 @@
 import type { RunOptions } from '../../types';
-import { $, runInRepo } from '../../utils';
+import { runInRepo } from '../../utils';
 
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     repo: 'web-infra-dev/rspress',
     branch: process.env.RSPRESS_REF ?? 'main',
-    beforeTest: async () => {
-      await $`pnpm exec playwright install --with-deps`;
-    },
     test: ['test'],
   });
 }
